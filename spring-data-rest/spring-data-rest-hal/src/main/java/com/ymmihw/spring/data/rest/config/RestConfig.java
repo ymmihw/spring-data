666ce4a39854a3c1d87639d6a4ced98ba38af598
wanghaoming <whimmy@126.com>
2020-05-26 11:26:00 +0800

    add validation-api

--- spring-data-rest/spring-data-rest-hal/src/main/java/com/ymmihw/spring/data/rest/config/RestConfig.java
+++ spring-data-rest/spring-data-rest-hal/src/main/java/com/ymmihw/spring/data/rest/config/RestConfig.java
@@ -4,13 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.validation.Validator;
 
 @Configuration
-public class RestConfig extends RepositoryRestConfigurerAdapter {
-
-  // access to global validator
+public class RestConfig implements RepositoryRestConfigurer {
   @Autowired
   @Lazy
   private Validator validator;
