1d6c1fa9937d0c54d22c803df59976efe05bac9b
wanghaoming <whimmy@126.com>
2020-05-26 11:06:38 +0800

    add @EnableCassandraRepositories

--- spring-data-cassandra/spring-data-cassandra-reactive/src/main/java/com/ymmihw/spring/data/cassandra/SpringDataCassandraReactiveApplication.java
+++ spring-data-cassandra/spring-data-cassandra-reactive/src/main/java/com/ymmihw/spring/data/cassandra/SpringDataCassandraReactiveApplication.java
@@ -2,8 +2,10 @@ package com.ymmihw.spring.data.cassandra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @SpringBootApplication
+@EnableCassandraRepositories(basePackages = {"com.ymmihw.spring.data.cassandra.repository"})
 public class SpringDataCassandraReactiveApplication {
 
   public static void main(String[] args) {
