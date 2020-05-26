8fc2fdedf977ad755113cf9d86d4a846206371d5
wanghaoming <whimmy@126.com>
2020-05-26 11:04:15 +0800

    add @Repository

--- spring-data-cassandra/spring-data-cassandra-reactive/src/main/java/com/ymmihw/spring/data/cassandra/repository/EmployeeRepository.java
+++ spring-data-cassandra/spring-data-cassandra-reactive/src/main/java/com/ymmihw/spring/data/cassandra/repository/EmployeeRepository.java
@@ -2,9 +2,11 @@ package com.ymmihw.spring.data.cassandra.repository;
 
 import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
+import org.springframework.stereotype.Repository;
 import com.ymmihw.spring.data.cassandra.model.Employee;
 import reactor.core.publisher.Flux;
 
+@Repository
 public interface EmployeeRepository extends ReactiveCassandraRepository<Employee, Integer> {
   @AllowFiltering
   Flux<Employee> findByAgeGreaterThan(int age);
