9eccd5ad5434564e2a64abed1ff6e1631de23f7b
wanghaoming <wanghaoming@ctfo.com>
2021-04-18 07:42:57 +0800

    fixed

--- spring-data-jpa/spring-data-jpa-entity-graph/src/test/java/com/ymmihw/spring/data/jpa/EntityGraphIntegrationTest.java
+++ spring-data-jpa/spring-data-jpa-entity-graph/src/test/java/com/ymmihw/spring/data/jpa/EntityGraphIntegrationTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import com.ymmihw.spring.data.jpa.model.Characteristic;
 import com.ymmihw.spring.data.jpa.model.Item;
@@ -12,7 +11,6 @@ import com.ymmihw.spring.data.jpa.repository.CharacteristicsRepository;
 import com.ymmihw.spring.data.jpa.repository.ItemRepository;
 
 @DataJpaTest
-@SpringBootTest
 @Sql(scripts = "/entitygraph-data.sql")
 public class EntityGraphIntegrationTest {
 
